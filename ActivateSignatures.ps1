73ab997b3b12c2f0dce9428b5ac639aba39825df
bb-froggy <ch-git@hannebauer.name>
2017-02-22 10:25:58 +0100

    More generalization

--- ActivateSignatures.ps1
+++ ActivateSignatures.ps1
@@ -1,14 +1,6 @@
 ﻿$CAName = "CN=COMODO SHA-256 Client Authentication and Secure Email CA, O=COMODO CA Limited, L=Salford, S=Greater Manchester, C=GB"
 
-cd cert:\CurrentUser\My
-$cert = (dir | ? { $_.Issuer -eq $CAName -and $_.HasPrivateKey -and $_.Verify() -and ($_.EnhancedKeyUsageList | ? { $_.ObjectId -eq "1.3.6.1.5.5.7.3.4" }) -ne $null }) # 1.3.6.1.5.5.7.3.4 is the OID of Secure Email
-
-if ($null -eq $cert) {
-    Write-Host "No certificate found to be used as signature certificate." 
-    Return 30
-}
-
-function CreateOutlookSignatureSettings($OfficeBitness) {
+function CreateOutlookSignatureSettings($OfficeBitness, $SigningCertificate, $EncryptionCertificate, $SettingsName = "Outlook Signature Settings") {
 
     ## Create detailed Settings
 
@@ -16,7 +8,6 @@ function CreateOutlookSignatureSettings($OfficeBitness) {
     $binPreliminaries = 0x01, 0x00, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x00, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20, 0x00, 0x08, 0x00, 0x07, 0x00, 0x00, 0x00
 
     ### Settings Name
-    $SettingsName = "Outlook Signature Settings"
 
     $binSettingsNameUnicode = [System.Text.Encoding]::Unicode.GetBytes($SettingsName)
     $binSettingsNameASCII = [System.Text.Encoding]::ASCII.GetBytes($SettingsName)
@@ -26,8 +17,13 @@ function CreateOutlookSignatureSettings($OfficeBitness) {
 
     ### Certificate Hashes
 
-    [byte[]]$BinThumbprintSettings = (9,0,0x18,0) + $cert.GetCertHash() # Signature certificate
-    # (0x22,0,0x18,0) + $cert.GetCertHash() # Encryption Certificate
+    [byte[]]$BinThumbprintSettings = @()
+    if ($null -ne $SigningCertificate) {
+        [byte[]]$BinThumbprintSettings += (9,0,0x18,0) + $SigningCertificate.GetCertHash()
+    }
+    if ($null -ne $EncryptionCertificate) {
+        [byte[]]$BinThumbprintSettings += (0x22,0,0x18,0) + $EncryptionCertificate.GetCertHash()
+    }
 
     ### Algorithms
     # Sha256 as Hash algorithm, it's ASN.1 encoded
@@ -55,14 +51,14 @@ function CreateOutlookSignatureSettings($OfficeBitness) {
     return $binSettingsTotal
 }
 
-function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath) {
+function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath, $SigningCertificate, $EncryptionCertificate) {
     $OfficeBitness = (Get-ItemProperty $OutlookHKLMPath).BitNess
     if ($null -eq $OfficeBitness) {
         Write-Host "Old version of Outlook detected, but it was deinstalled already. This is usually okay." 
         Return 50
     }
 
-    $key = (Get-ItemProperty $OutlookSettingsPath).{00030354}
+    $key = (Get-ItemProperty $OutlookSettingsPath).{11020355}
     if ($null -ne $key) {
         Write-Host "Security Settings already exist." 
         Return 20
@@ -70,34 +66,62 @@ function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath) {
 
     Write-Host "Configuring Outlook in Path $OutlookSettingsPath" 
 
-    [byte[]] $binSettings = CreateOutlookSignatureSettings $OfficeBitness
+    [byte[]] $binSettings = CreateOutlookSignatureSettings $OfficeBitness $SigningCertificate $EncryptionCertificate
     $dummy = New-ItemProperty $OutlookSettingsPath -Name 11020355 -PropertyType Binary -Value $binSettings
 
-    ## Enable Signatures
-    [byte[]]$binCryptoActivation = 2,0,0,0   # Signature enabled (2), Encryption disabled (not 1)
-    $dummy = New-ItemProperty $OutlookSettingsPath -Name 00030354 -PropertyType Binary -Value $binCryptoActivation
+    [byte]$CryptoEnablerBits = 0
+    if ($null -ne $EncryptionCertificate) {
+        [byte]$CryptoEnablerBits = $CryptoEnablerBits -bor 0x01 # Bit 0 (LSB) indicates whether encryption is enabled
+    }
+    if ($null -ne $SigningCertificate) {
+        [byte]$CryptoEnablerBits = $CryptoEnablerBits -bor 0x02 # Bit 1 indicates whether signing is enabled
+    }
+
+    ## Enable Signatures and/or Encryption as default
+    [byte[]]$binCryptoActivation = $CryptoEnablerBits,0,0,0
+    $dummy = New-ItemProperty $OutlookSettingsPath -Name 00030354 -PropertyType Binary -Value $binCryptoActivation -Force
 }
 
+
+# Main
+
+## Search for an appropriate certificate
+cd cert:\CurrentUser\My
+$sOidSecureEmail = "1.3.6.1.5.5.7.3.4"
+$cert = (dir | ? { $_.Issuer -eq $CAName -and $_.HasPrivateKey -and $_.Verify() -and ($_.EnhancedKeyUsageList | ? { $_.ObjectId -eq $sOidSecureEmail }) -ne $null })
+# REVISIT: If multiple suitable certificates are found, better use the one that expires last
+
+if ($null -eq $cert) {
+    Write-Host "No certificate found to be used as signature certificate." 
+    Return 30
+}
+
+## Configure Outlook 2016 (32 Bit and 64 Bit)
+### Either 64 Bit on 64 Bit machine or 32 Bit on 32 Bit machine
 if (Test-Path HKLM:\SOFTWARE\Microsoft\Office\16.0\Outlook) {
     $DefaultProfile = (Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook).DefaultProfile
 
-    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\Microsoft\Office\16.0\Outlook
+    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\Microsoft\Office\16.0\Outlook $cert $null
 }
 
+### 32 Bit Outlook on 64 Bit Windows
 if (Test-Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\16.0\Outlook) {
     $DefaultProfile = (Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook).DefaultProfile
 
-    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\16.0\Outlook
+    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\16.0\Outlook $cert $null
 }
 
+## Configure Outlook 2013 (32 Bit and 64 Bit)
+### Either 64 Bit on 64 Bit machine or 32 Bit on 32 Bit machine
 if (Test-Path HKLM:\SOFTWARE\Microsoft\Office\15.0\Outlook) {
     $DefaultProfile = (Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook).DefaultProfile
 
-    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\Microsoft\Office\15.0\Outlook
+    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\Microsoft\Office\15.0\Outlook $cert $null
 }
 
+### 32 Bit Outlook on 64 Bit Windows
 if (Test-Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\15.0\Outlook) {
     $DefaultProfile = (Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook).DefaultProfile
 
-    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\15.0\Outlook
+    ConfigureOutlookSignatures "HKCU:\SOFTWARE\Microsoft\Office\15.0\Outlook\Profiles\$DefaultProfile\c02ebc5353d9cd11975200aa004ae40e" HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\15.0\Outlook $cert $null
 }
