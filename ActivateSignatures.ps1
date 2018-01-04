00d17b0d28e9f3911d2ad073a98a35f39bea501c
bb-froggy <ch-git@hannebauer.name>
2018-01-04 14:48:20 +0100

    Parses existing configuration and overwrites settings if the configured certificate is not valid anymore

--- ActivateSignatures.ps1
+++ ActivateSignatures.ps1
@@ -20,14 +20,12 @@ This enables the Sign and encrypt buttons of the Options dialogue when composing
 	.EXAMPLE
     .\ActivateSignatures.ps1 -CAName "CN=COMODO RSA Client Authentication and Secure Email CA, O=COMODO CA Limited, L=Salford, S=Greater Manchester, C=GB" -AlwaysSignMails
 
-Limitations:
-	- Script only configures the certificate used once and will not select a renewed certificate, except if using the Force parameter
-	
 Changelog:
 20170313: Outlook2010, Win2008 EnhancedKeyUsage detection, $AlwaysSignMails, Parameters - T. Kunzi
 20170512: Fixed Problems with older Powershell/.NET versions - C. Hannebauer
 20170512b: More output (for debugging) - C. Hannebauer
 20180104: Force switch and backups - C. Hannebauer
+20180104b: Parses existing configuration and overwrites settings if the configured certificate is not valid anymore - C. Hannebauer
 
 #>
 param
@@ -37,55 +35,193 @@ param
 [Parameter(Position=2,Mandatory=$false,ValueFromPipeline=$false,HelpMessage='always write new configuration data, even if some configuration data exists already (for example using an expired certificate)')][switch]$Force
 )
 
-function CreateOutlookSignatureSettings($OfficeBitness, $SigningCertificate, $EncryptionCertificate, $SettingsName = "Outlook Signature Settings") {
+Class OutlookSignatureSettings {
 
-    ## Create detailed Settings
+    [string] $SettingsName;
+    [string] $OfficeBitness; # Can only be x86 or x64
+    [byte[]] $SigningCertificateHash;
+    [byte[]] $EncryptionCertificateHash;
 
-    ### Some preliminary stuff (exact meaning not tested)
-    $binPreliminaries = 0x01, 0x00, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x00, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20, 0x00, 0x08, 0x00, 0x07, 0x00, 0x00, 0x00
-
-    ### Settings Name
-
-    $binSettingsNameUnicode = [System.Text.Encoding]::Unicode.GetBytes($SettingsName)
-    $binSettingsNameASCII = [System.Text.Encoding]::ASCII.GetBytes($SettingsName)
-
-    [byte[]]$binSettingsName = (0x51,00,($binSettingsNameUnicode.Length+6),00) + $binSettingsNameUnicode + (00, 00)
-    $binSettingsName += (0x0b,00,($binSettingsNameASCII.Length+5),00) + $binSettingsNameASCII + (00)
-
-    ### Certificate Hashes
-
-    [byte[]]$BinThumbprintSettings = @()
-    if ($null -ne $SigningCertificate) {
-        [byte[]]$BinThumbprintSettings += (9,0,0x18,0) + $SigningCertificate.GetCertHash()
-    }
-    if ($null -ne $EncryptionCertificate) {
-        [byte[]]$BinThumbprintSettings += (0x22,0,0x18,0) + $EncryptionCertificate.GetCertHash()
+    OutlookSignatureSettings([string] $OfficeBitness, [System.Security.Cryptography.X509Certificates.X509Certificate2] $SigningCertificate, [System.Security.Cryptography.X509Certificates.X509Certificate2] $EncryptionCertificate) {
+        
+        $this.SettingsName = "Outlook Signature Settings";
+        $this.OfficeBitness = $OfficeBitness;
+        if ($null -ne $SigningCertificate) {
+            $this.SigningCertificateHash = $SigningCertificate.GetCertHash();
+        }
+        if ($null -ne $EncryptionCertificate) {
+            $this.EncryptionCertificateHash = $EncryptionCertificate.GetCertHash();
+        }
     }
 
-    ### Algorithms
-    # Sha256 as Hash algorithm, it's ASN.1 encoded
-    [byte[]] $binAlgorithms = 0x02, 0x00, 0x36, 0x00, 0x30, 0x30, 0x30, 0x0B, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x30, 0x0B, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03, 0x30, 0x0B, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x02, 0x30, 0x07, 0x06, 0x05, 0x2B, 0x0E, 0x03, 0x02, 0x1A
+    OutlookSignatureSettings([string] $OfficeBitness, [byte[]] $binExistingSettings) {
+        $this.OfficeBitness = $OfficeBitness
 
-    ### Total
+        if ($binExistingSettings.Length -le 20) {
+            throw "Existing settings are only $($binExistingSettings.Length) bytes long, too short for real settings.";
+        }
+        if (($binExistingSettings.Length % 16) -ne 0) {
+            throw "Existing settings are $($binExistingSettings.Length) bytes long, which is not dividable by 16. Real settings are always padded to multiples of 16.";
+        }
 
-    [byte[]] $binSettingsBody = $binPreliminaries + $binSettingsName + $BinThumbprintSettings + $binAlgorithms
-    if ($OfficeBitness -eq "x86") {
-	    [byte[]] $binSettingsHeader = (1,0,0,0) + [System.BitConverter]::GetBytes($binSettingsBody.Length) + `
-		    [System.BitConverter]::GetBytes(12) # Length of the Header in Bytes
+        if (1 -ne ([System.BitConverter]::ToUInt32($binExistingSettings,0))) {
+            throw "Existing settings do not start with 01 00 00 00 as expected";
+        }
+        
+        if ($this.OfficeBitness -eq "x86") {
+            $lengthOfBody = [System.BitConverter]::ToUInt32($binExistingSettings,4)
+            $lengthOfHeader = [System.BitConverter]::ToUInt32($binExistingSettings,8)
+            if ($lengthOfHeader -ne 12) {
+                throw "Header length encoded to $lengthOfHeader instead of the expected 12";
+            }
+        }
+        ElseIf ($this.OfficeBitness -eq "x64") {
+            $lengthOfBody = [System.BitConverter]::ToUInt64($binExistingSettings,4)
+            $lengthOfHeader = [System.BitConverter]::ToUInt64($binExistingSettings,12)
+            if ($lengthOfHeader -ne 20) {
+                throw "Header length encoded to $lengthOfHeader instead of the expected 20";
+            }
+        }
+        Else {
+            throw [System.ArgumentException] "Unknown Office Bitness: $this.OfficeBitness"
+        }
+
+        $totalLength = $lengthOfBody + $lengthOfHeader;
+        $totalLength += (16 - $totalLength % 16)
+        if ($totalLength -ne $binExistingSettings.Length) {
+            throw "Encoding says the header has a length of $lengthOfHeader bytes, while the body's length should be $lengthOfBody. Together with padding, this should be $totalLength bytes. However, the real length is different, specifically $binExistingSettings.Length bytes.";
+        }
+
+        $currentPosition = $lengthOfHeader
+
+        do {
+            $packetTag = [BitConverter]::ToUInt16($binExistingSettings, $currentPosition);
+            $packetLength = [BitConverter]::ToUInt16($binExistingSettings, $currentPosition + 2);
+
+            switch($packetTag) {
+                0x51 {
+                    $this.SettingsName = [System.Text.Encoding]::Unicode.GetString($binExistingSettings, $currentPosition + 4, $packetLength - 4);
+                }
+                0x0b {                    # ASCII name, doesn't need to be parsed
+                }
+                0x01 {
+                    # some value of the preliminaries of unknown purpose
+                }
+                0x06 {
+                    # some value of the preliminaries of unknown purpose
+                }
+                0x20 {
+                    # some value of the preliminaries of unknown purpose
+                }
+                0x02 {                    # Hash algorithm, doesn't need to be parsed
+                }
+                0x09 {                    # Signing certificate hash
+                    $this.SigningCertificateHash = for($i = 4; $i -lt $packetLength; $i++) { $binExistingSettings[$currentPosition + $i] }
+                }
+                0x22 {                      # Encryption certificate hash
+                    $this.EncryptionCertificateHash = for($i = 4; $i -lt $packetLength; $i++) { $binExistingSettings[$currentPosition + $i] }
+                }
+                default { Write-Debug "Unknown Packet Tag $packetTag" }
+            }
+
+            $currentPosition += $packetLength;
+            if ($currentPosition + 4 -gt $binExistingSettings.Length) {
+                $packetLength = 0; # Force stop, there is nothing left to read
+            }
+        } while($packetLength -gt 0) # A packet of length null is actually part of the padding already
     }
-    ElseIf ($OfficeBitness -eq "x64") {
-	    [byte[]] $binSettingsHeader = (1,0,0,0) + [System.BitConverter]::GetBytes($binSettingsBody.LongLength) + `
-		    [System.BitConverter]::GetBytes([long]20) # Length of the Header in Bytes
-    }
-    Else {
-        throw [System.ArgumentException] "Unknown Office Bitness: $OfficeBitness"
+
+    [System.Security.Cryptography.X509Certificates.X509Certificate2] FindSigningCertificate() {
+        if ($this.SigningCertificateHash -eq $null) { return $null; }
+        
+        return dir cert:\CurrentUser\My | ? { @(Compare-Object $_.GetCertHash() $this.SigningCertificateHash -sync 0).Length -eq 0}
     }
 
-    # Padding to 16 Byte block
-    [byte[]] $binSettingsTotal = $binSettingsHeader + $binSettingsBody
-    $binSettingsTotal += @(0) * (16 - $binSettingsTotal.Length % 16)
+    [System.Security.Cryptography.X509Certificates.X509Certificate2] FindEncryptionCertificate() {
+    if ($this.EncryptionCertificateHash -eq $null) { return $null; }
+        
+        return dir cert:\CurrentUser\My | ? { @(Compare-Object $_.GetCertHash() $this.EncryptionCertificateHash -sync 0).Length -eq 0}
+    }
 
-    return $binSettingsTotal
+    [bool] ValidateSettings() { # Checks whether configured certificates exist and are still valid. If not, return false.
+
+        if ($this.SigningCertificateHash -ne $null) {
+            $configuredSigningCertificate = dir cert:\CurrentUser\My | ? { @(Compare-Object $_.GetCertHash() $this.SigningCertificateHash -sync 0).Length -eq 0}
+            if ($configuredSigningCertificate -eq $null) {
+                Write-Information "Signing Certificate is configured, but does not exist"
+                return $false;
+            }
+            if (-not $configuredSigningCertificate.Verify()) {
+                Write-Information "Signing Certificate is configured, but is not valid"
+                return $false;
+            }
+        }
+
+        if ($this.EncryptionCertificateHash -ne $null) {
+            $configuredEncryptionCertificate = dir cert:\CurrentUser\My | ? { @(Compare-Object $_.GetCertHash() $this.EncryptionCertificateHash -sync 0).Length -eq 0}
+            if ($configuredEncryptionCertificate -eq $null) {
+                Write-Information "Encryption Certificate is configured, but does not exist"
+                return $false;
+            }
+            if (-not $configuredEncryptionCertificate.Verify()) {
+                Write-Information "Encryption Certificate is configured, but is not valid"
+                return $false;
+            }
+        }
+
+        return $true
+    }
+
+    [byte[]] CreateRegistryValue() {
+
+        ## Create detailed Settings
+        
+        ### Some preliminary stuff (exact meaning not tested)
+        [byte[]] $binPreliminaries = 0x01, 0x00, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x00, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20, 0x00, 0x08, 0x00, 0x07, 0x00, 0x00, 0x00
+
+        ### Settings Name
+
+        $binSettingsNameUnicode = [System.Text.Encoding]::Unicode.GetBytes($this.SettingsName)
+        $binSettingsNameASCII = [System.Text.Encoding]::ASCII.GetBytes($this.SettingsName)
+
+        [byte[]]$binSettingsName = (0x51,00,($binSettingsNameUnicode.Length+6),00) + $binSettingsNameUnicode + (00, 00)
+        $binSettingsName += (0x0b,00,($binSettingsNameASCII.Length+5),00) + $binSettingsNameASCII + (00)
+
+        ### Certificate Hashes
+
+        [byte[]]$BinThumbprintSettings = @()
+        if ($null -ne $this.SigningCertificateHash) {
+            [byte[]]$BinThumbprintSettings += (9,0,0x18,0) + $this.SigningCertificateHash
+        }
+        if ($null -ne $this.EncryptionCertificateHash) {
+            [byte[]]$BinThumbprintSettings += (0x22,0,0x18,0) + $this.EncryptionCertificateHash
+        }
+
+        ### Algorithms
+        # Sha256 as Hash algorithm, it's ASN.1 encoded
+        [byte[]] $binAlgorithms = 0x02, 0x00, 0x36, 0x00, 0x30, 0x30, 0x30, 0x0B, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x30, 0x0B, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03, 0x30, 0x0B, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x02, 0x30, 0x07, 0x06, 0x05, 0x2B, 0x0E, 0x03, 0x02, 0x1A
+
+        ### Total
+
+        [byte[]] $binSettingsBody = $binPreliminaries + $binSettingsName + $BinThumbprintSettings + $binAlgorithms
+        if ($this.OfficeBitness -eq "x86") {
+	        [byte[]] $binSettingsHeader = (1,0,0,0) + [System.BitConverter]::GetBytes($binSettingsBody.Length) + `
+		        [System.BitConverter]::GetBytes(12) # Length of the Header in Bytes
+        }
+        ElseIf ($this.OfficeBitness -eq "x64") {
+	        [byte[]] $binSettingsHeader = (1,0,0,0) + [System.BitConverter]::GetBytes($binSettingsBody.LongLength) + `
+		        [System.BitConverter]::GetBytes([long]20) # Length of the Header in Bytes
+        }
+        Else {
+            throw [System.ArgumentException] "Unknown Office Bitness: $this.OfficeBitness"
+        }
+
+        # Padding to 16 Byte block
+        [byte[]] $binSettingsTotal = $binSettingsHeader + $binSettingsBody
+        $binSettingsTotal += @(0) * (16 - $binSettingsTotal.Length % 16)
+
+        return $binSettingsTotal
+    }
 }
 
 function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath, $SigningCertificate, $EncryptionCertificate) {
@@ -101,18 +237,29 @@ function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath, $Sig
         $key = (Get-ItemProperty $OutlookSettingsPath).{11020355}
         if ($null -ne $key) {
             if ($Force) {
-                $CurrentDate = Get-Date -Format "s"
-                $backupPath = "$OutlookSettingsPath-backups\scriptbackup-$CurrentDate"
-                Write-Information "Security Settings already exist, but continuing due to Force parameter. Creating a backup of the registry key now."
-                if (-not (Test-Path "$OutlookSettingsPath-backups")) {
-                    New-Item -Path "$OutlookSettingsPath-backups" -ItemType RegistryKey -Force
-                }
-                copy $OutlookSettingsPath $backupPath -Force -ErrorAction Stop
+                Write-Information "Security Settings already exist, but continuing due to Force parameter."
             }
             else {
-                Write-Error "Security Settings already exist. Use Force parameter if you still want to write new settings." 
-                Return 20
+                $binExistingSettings = Get-ItemPropertyValue -Path $OutlookSettingsPath -Name 11020355
+                $objExistingSettings = New-Object OutlookSignatureSettings($OfficeBitness, $binExistingSettings)
+
+                if ($objExistingSettings.ValidateSettings()) {
+                    Write-Error "Valid Security Settings already exist. Use Force parameter if you still want to write new settings." 
+                    Return 20
+                }
+                else {
+                    Write-Information "Security Settings exist already, but they are not valid anymore (an non-existing or expired certificate was configured). Rewriting configuration..."
+                }
             }
+            
+            Write-Information "Creating a backup of the registry key with existing Security Settings now."
+
+            $CurrentDate = Get-Date -Format "s"
+            $backupPath = "$OutlookSettingsPath-backups\scriptbackup-$CurrentDate"
+            if (-not (Test-Path "$OutlookSettingsPath-backups")) {
+                New-Item -Path "$OutlookSettingsPath-backups" -ItemType RegistryKey -Force
+            }
+            copy $OutlookSettingsPath $backupPath -Force -ErrorAction Stop
         }
     }
     else { # The whole registry key might not exist if Outlook has never touched security settings
@@ -122,8 +269,9 @@ function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath, $Sig
     Write-Information "Configuring Outlook in Path $OutlookSettingsPath" 
 
     try {
-        [byte[]] $binSettings = CreateOutlookSignatureSettings $OfficeBitness $SigningCertificate $EncryptionCertificate
-        $dummy = New-ItemProperty $OutlookSettingsPath -Name 11020355 -PropertyType Binary -Value $binSettings -Force:$Force.IsPresent
+        $outlookCertificateConfiguration = new-object OutlookSignatureSettings($OfficeBitness, $SigningCertificate, $EncryptionCertificate)
+        [byte[]] $binSettings = $outlookCertificateConfiguration.CreateRegistryValue()
+        $dummy = New-ItemProperty $OutlookSettingsPath -Name 11020355 -PropertyType Binary -Value $binSettings -Force:(-not [String]::IsNullOrEmpty($backupPath))
 
         [byte]$CryptoEnablerBits = 0
         if ($null -ne $EncryptionCertificate) {
@@ -152,11 +300,12 @@ function ConfigureOutlookSignatures($OutlookSettingsPath, $OutlookHKLMPath, $Sig
             Write-Information "copying backuped value to the original registry location"
             copy $backupPath $OutlookSettingsPath
         }
+        throw
     }
 }
 
 # Main
-Write-Information "ActivateSignatures Version 20180104"
+Write-Information "ActivateSignatures Version 20180104b"
 
 ## Search for an appropriate certificate
 $sOidSecureEmail = "1.3.6.1.5.5.7.3.4"
