e1d9d94d318841a456bdf392f6672b8669ebe4cf
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2022-05-02 11:14:25 +0200

    Handling more cases

--- Export-SMIMECertificate.ps1
+++ Export-SMIMECertificate.ps1
@@ -7,14 +7,20 @@ Searches for S/MIME encryption certificates in the user store. All found certifi
     Where to export the certificates to, e.g. "%OneDriveCommercial%\smimecerts.pfx"
 	.PARAMETER Password
     The password with which the exported PFX files will be protected.	
-	
+	.PARAMETER CAName
+    Optionally, name of the Issuing CA of the certificates to be exported. You must use this exact spacing scheme:
+	- No spaces before and after the "=" (except if the actual value contains spaces, naturally)
+	- A single space after each comma separating Subject DN components
+    
 	.EXAMPLE
     .\Export-SMIMECertificate.ps1 -Path "%OneDriveCommercial%\smimecerts.pfx" -Password "password" -CAName "CN=COMODO RSA Client Authentication and Secure Email CA, O=COMODO CA Limited, L=Salford, S=Greater Manchester, C=GB"
 #>
 param
 ( 
 [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$false,HelpMessage='the file path to export the certificates to')][string]$Path,
-[Parameter(Position=1,Mandatory=$false,ValueFromPipeline=$false,HelpMessage='the password with which the exported PFX files will be protected')][string]$Password
+[Parameter(Position=1,Mandatory=$true,ValueFromPipeline=$false,HelpMessage='the password with which the exported PFX files will be protected')][string]$Password,
+[Parameter(Position=0,Mandatory=$false,ValueFromPipeline=$false,HelpMessage='exact name of Issuing CA')][string]$CAName
+
 )
 
 ## Search for S/MIME certificates
@@ -24,10 +30,19 @@ Write-Information "There are $($CandidateCerts.Length) certificates for S/MIME"
 
 $sOidKeyUsageExtension = "2.5.29.15"
 $encryptionCertificates = $CandidateCerts | Where-Object { ($null -eq ($_.Extensions | Where-Object { $_.Oid -eq $sOidKeyUsageExtension })) -OR ($null -ne ($_.Extensions | Where-Object { $_.Oid -eq $sOidKeyUsageExtension -AND $_.KeyUsages.HasFlag([System.Security.Cryptography.X509Certificates.X509KeyUsageFlags]::KeyEncipherment)})) }
+Write-Information "Of these, $($encryptionCertificates.Length) are encryption certificates"
 
-$certs4export = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
-$certs4export.AddRange($encryptionCertificates)
-$baPFX = $certs4export.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $Password)
+if ($null -ne $CAName) {
+    $encryptionCertificates = $encryptionCertificates | Where-Object { $_.Issuer -eq $CAName }
+    Write-Information "Of these, $($encryptionCertificates.Length) were issued by $CAName"
+}
 
-$expandedPath = [System.Environment]::ExpandEnvironmentVariables($Path)
-[System.IO.File]::WriteAllBytes($expandedPath, $baPFX)
+if ($null -ne $encryptionCertificates) {
+    $certs4export = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+    $certs4export.AddRange($encryptionCertificates)
+    $baPFX = $certs4export.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $Password)
+
+    $expandedPath = [System.Environment]::ExpandEnvironmentVariables($Path)
+    [System.IO.File]::WriteAllBytes($expandedPath, $baPFX)
+    Write-Information "PFX written to $expandedPath"
+}
